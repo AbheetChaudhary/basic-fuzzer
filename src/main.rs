61949ed4efafa0eaefaa24416c452a20e2eb3351
Abheet Chaudhary <81126136+AbheetChaudhary@users.noreply.github.com>
2024-04-02 23:49:26 +0530

    Reduce unnecessary allocation

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,10 @@ use std::process::{Command, Stdio};
 use std::sync::{mpsc, Arc, Mutex};
 use std::thread;
 use std::time;
+use std::os::unix::process::ExitStatusExt;
 
-const MAX_THREADS: usize = 8;
+const MAX_THREADS: usize = 4; // no. of worker threads
+const MUTATIONS: usize = 1e5 as usize; // no. of fuzz cases each thread has to process
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let corpus_names = fs::read_dir("corpus")
@@ -32,7 +34,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let corpus = Arc::new(corpus);
 
     let pool = ThreadPool::new(MAX_THREADS);
-    for _ in 0..4 * 1e5 as usize {
+    for _ in 0..MAX_THREADS * MUTATIONS {
         pool.sender
             .as_ref()
             .unwrap()
@@ -75,19 +77,22 @@ impl Worker {
     fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Data>>>) -> Self {
         let start_instant = time::Instant::now();
         let mut count = 0;
+        let mut fuzz_inp = Vec::new();
         let thread = thread::spawn(move || loop {
             let corpus = receiver.lock().unwrap().recv();
             match corpus {
                 Ok(corpus) => {
                     // let input = job();
-                    let mut inp = corpus.choose(&mut rand::thread_rng()).unwrap().clone();
-                    let inp_len = inp.len();
+                    let inp_slice = &corpus.choose(&mut rand::thread_rng()).unwrap()[..];
+                    fuzz_inp.clear(); // no need to allocate new for each fuzz case
+                    fuzz_inp.extend_from_slice(inp_slice);
+                    let inp_len = fuzz_inp.len();
                     for _ in 0..rand::thread_rng().gen_range(0..32) {
-                        inp[rand::thread_rng().gen_range(0..inp_len)] =
+                        fuzz_inp[rand::thread_rng().gen_range(0..inp_len)] =
                             rand::thread_rng().gen_range(0..255);
                     }
                     let tmpfn = format!("tmp_{id:02}");
-                    fs::write(&tmpfn[..], &inp[..]).unwrap();
+                    fs::write(&tmpfn[..], &fuzz_inp[..]).unwrap();
 
                     let output = Command::new("objdump")
                         .arg("-x")
@@ -102,12 +107,14 @@ impl Worker {
                     if output.status.success() {
                         count += 1;
                         println!(
-                            "worker_{id:02}\ttime: {:#?}\tworker case: {count:010} | fcps: {:10.4}",
-                            delta_t,
+                            "worker_{id:02}\ttime: {:010.4}\tworker case: {count:010} | fcps: {:10.4}",
+                            delta_t.as_secs_f32(),
                             count as f64 / delta_t.as_secs() as f64
                         );
                     } else {
-                        println!("Exited with status {}", output.status.code().unwrap());
+                        if let Some(11) = output.status.signal() {
+                            println!("SIGSEV");
+                        }
                     };
                 }
                 Err(_) => {
